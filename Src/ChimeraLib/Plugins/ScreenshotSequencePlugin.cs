e1a42ff94cf10d3fd00560bbc67656235961a013
adeolafabola <aef6@st-andrews.ac.uk>
2015-03-26 10:49:13 +0000

    Screenshotsequenceplugin.cs modified to capture panoramas periodically

--- Src/ChimeraLib/Plugins/ScreenshotSequencePlugin.cs
+++ Src/ChimeraLib/Plugins/ScreenshotSequencePlugin.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Chimera.Config;
-using System.Windows.Forms;
 using Chimera.GUI.Controls.Plugins;
+using OpenMetaverse;
+using Chimera.Util;
 using System.Drawing;
-using System.IO;
 using System.Threading;
+using System.IO;
+using log4net;
+using System.Windows.Forms;
+
 
 namespace Chimera.Plugins
 {
@@ -23,8 +27,13 @@ namespace Chimera.Plugins
         private DateTime mLastPress;
         private Action mTickListener;
         private Frame mFrame;
+        private int mW, mH;
+        private int step = 0;
+        private String folder;
 
         private Queue<Bitmap> mScreenshots = new Queue<Bitmap>();
+
+        private ILog Logger = LogManager.GetLogger("Panorama");
 
         public event Action Started;
         public event Action Stopped;
@@ -39,11 +48,14 @@ namespace Chimera.Plugins
                     mRunning = value;
                     if (value)
                     {
+                        mW = mFrame.Monitor.Bounds.Width;
+                        mH = mFrame.Monitor.Bounds.Height;
+
                         mStarted = DateTime.Now;
                         mCore.Tick += mTickListener;
-                        Thread t = new Thread(ScreenshotProcessor);
-                        t.Name = "Screenshot Processor";
-                        t.Start();
+                        //Thread t = new Thread(ScreenshotProcessor);
+                        //t.Name = "Screenshot Processor";
+                        //t.Start();
                         if (Started != null)
                             Started();
                     }
@@ -124,9 +136,50 @@ namespace Chimera.Plugins
             }
             else if (mConfig.IntervalMS < DateTime.Now.Subtract(mLastPress).TotalMilliseconds)
             {
-                TakeScreenshot();
+                new Thread(TakePanorama).Start();
                 mLastPress = DateTime.Now;
             }
+        }
+
+        public void TakePanorama()
+        {
+            step++;
+            folder = Path.Combine(mConfig.ScreenshotFolder, "pano"+step);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            Rotation r = mCore.Orientation;
+            mRunning = true;
+
+            /*
+              if (!Directory.Exists(mConfig.ScreenshotFolder))
+                Directory.CreateDirectory(mConfig.ScreenshotFolder);
+            */
+            Thread tp = new Thread(ScreenshotProcessor);
+            tp.Name = "Panorama Image Processor";
+            tp.Start();
+
+            for (int i = 1; i < 7; i++)
+            {
+                mCore.Update(mCore.Position, Vector3.Zero, GetRotation(i), Rotation.Zero);
+                Thread.Sleep(mConfig.CaptureDelayMS);
+                TakeScreenshot();
+            }
+
+            mRunning = false;
+            mCore.Update(mCore.Position, Vector3.Zero, r, Rotation.Zero);
+        }
+
+        private void TakeScreenshot()
+        {
+            Bitmap screenshot = new Bitmap(mW, mH);
+            using (Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(mFrame.Monitor.Bounds.Location, Point.Empty, mFrame.Monitor.Bounds.Size);
+            }
+            mScreenshots.Enqueue(screenshot);
         }
 
         private void ScreenshotProcessor()
@@ -137,7 +190,13 @@ namespace Chimera.Plugins
                 if (mScreenshots.Count > 0)
                 {
                     Bitmap screenshot = mScreenshots.Dequeue();
-                    screenshot.Save(Path.Combine(mConfig.ScreenshotFolder, mConfig.ScreenshotFile) + "_" + (image++) + ".png");
+                    using (Bitmap resized = new Bitmap(screenshot, new Size(screenshot.Height, screenshot.Height)))
+                    {
+                        //string file = Path.Combine(mConfig.ScreenshotFolder, step + GetImageName(image++) + ".png")
+                        string file = Path.Combine(folder, GetImageName(image++) + ".png");
+                        Logger.Info("Writing Panorama image to: " + file + ".");
+                        resized.Save(file);
+                    }
                     screenshot.Dispose();
                 }
                 Thread.Sleep(5);
@@ -146,14 +205,32 @@ namespace Chimera.Plugins
                 mForm.Close();
         }
 
-        private void TakeScreenshot()
+        private Rotation GetRotation(int image)
         {
-            Bitmap screenshot = new Bitmap(mFrame.Monitor.Bounds.Width, mFrame.Monitor.Bounds.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
+            switch (image)
             {
-                g.CopyFromScreen(mFrame.Monitor.Bounds.Location, Point.Empty, mFrame.Monitor.Bounds.Size);
+                case 1: return new Rotation(0.0, 0.0);
+                case 2: return new Rotation(0.0, 90);
+                case 3: return new Rotation(0.0, 180.0);
+                case 4: return new Rotation(0.0, -90);
+                case 5: return new Rotation(-90.0, 0.0);
+                case 6: return new Rotation(90.0, 0.0);
+                default: return new Rotation(0.0, 0.0);
             }
-            mScreenshots.Enqueue(screenshot);
+        }
+
+        private string GetImageName(int image)
+        {
+            switch (image)
+            {
+                case 1: return "front";
+                case 2: return "right";
+                case 3: return "back";
+                case 4: return "left";
+                case 5: return "up";
+                case 6: return "down";
+                default: return "Unknown";
+            }
         }
     }
 }
